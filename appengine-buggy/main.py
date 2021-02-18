061eb852010b1231843109c1d15a2059068d5c63
Tim Berry <timhberry@gmail.com>
2021-02-18 13:09:37 +0000

    Add App Engine buggy demo

---
+++ appengine-buggy/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+try:
+  import googleclouddebugger
+  googleclouddebugger.enable(
+    breakpoint_enable_canary=True
+  )
+except ImportError:
+  pass
+
+
+@app.route('/')
+def index():
+    # only set value of sample_text 70% of the time
+    random_value = random()
+    if random_value < 0.5:
+        sample_text = "This app was written in Python 3. It will fail half of the time."
+    return render_template('index.html', sample_text=sample_text)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
